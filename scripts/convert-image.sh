fb3d8b62c1b94e2b0200ed67757d4a34eecc9b35
Aric Horstman <aric@nulldirective.com>
2016-01-30 20:51:47 -0800

    Added batch conversion

--- scripts/convert-image.sh
+++ scripts/convert-image.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-convert 20131109121409.tif -deskew 40% -background white -level 10%,70%,1 -blur 2 +dither +repage +matte -compress Group4 -colorspace gray -format tiff final.tiff
+
+mkdir -p ./output
+
+for file in ./*.tiff
+do
+  filename=$(basename "$file")
+  filename="${filename%.*}"
+
+  convert $file -deskew 40% -background white -level 10%,70%,1 -blur 2 +dither +repage +matte -compress Group4 -colorspace gray -format tiff output/$filename.tiff
+done
