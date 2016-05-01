c799a384016269dc11bab507d001eec9b6460e09
Rachel Aurand <rachel@countableset.com>
2016-05-01 12:34:32 -0700

    scan flow bash file

---
+++ scripts/scan.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+PAGECOUNTER=1
+FILES=()
+SCANFILES=()
+
+############################
+
+answer="y"
+while true
+do
+  case $answer in
+    [yY]* ) text="Scaning page "
+            text+=$PAGECOUNTER
+            echo $text
+            PAGECOUNTER=$[$PAGECOUNTER+1]
+
+            filename=`date +"%Y%m%d%H%M%S"`
+            filename+=".tiff"
+            FILES+=($filename)
+
+            scanimage --device-name 'genesys:libusb:001:006' --mode Color --resolution 300 -x 215.9 -y 279.4 --format=tiff -p > $filename
+            ;;
+    [nN]* ) break;;
+    * ) ;;
+  esac
+
+  read -p "Scan more pages? [y/n] " answer
+done
+
+############################
+
+SCANCOUNTER=1
+PREFIX="scan"
+for file in "${FILES[@]}"
+do
+  outputfilename=$PREFIX$(printf "%03d.tiff" "$SCANCOUNTER")
+  SCANFILES+=($outputfilename)
+  SCANCOUNTER=$[$SCANCOUNTER+1]
+
+  convert $file -deskew 40% -background white -level 10%,70%,1 -blur 2 +dither +repage +matte -compress Group4 -colorspace gray -format tiff $outputfilename
+done
+
+############################
+
+read -p "Enter pdf filename, no extension: " answer
+
+if [ -z "$answer" ]; then
+  answer=`date +"%Y%m%d%H%M%S"`
+fi
+
+tiffcp scan???.tiff doc.tiff
+tiff2pdf -j -o "$answer.pdf" doc.tiff
+
+############################
+# remove intermedite files
+for i in "${FILES[@]}"
+do
+  rm -f $i
+done
+for i in "${SCANFILES[@]}"
+do
+  rm -f $i
+done
+rm -f doc.tiff
