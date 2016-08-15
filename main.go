b901a6f546f4fa14a87feed731e367979c14e6ae
Rachel Aurand <rachel@countableset.com>
2016-08-14 20:25:20 -0700

    convert bash script to go version for better testsing

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var (
+	files     []string
+	scanfiles []string
+	yesre     = regexp.MustCompile("y(es)?")
+	nore      = regexp.MustCompile("n(o)?")
+	datere    = regexp.MustCompile("^([0-9]{4}(-[0-9]{2}){1,2}).*")
+)
+
+func main() {
+	startScanning()
+	filename := promptForFilename(os.Stdin)
+	convertFilesIntoNewTiffFiles()
+	createFinalPdfDocument(filename)
+	removeIntermediateFiles()
+}
+
+func runCommand(cmd *exec.Cmd) {
+	fmt.Println(strings.Join(cmd.Args, " "))
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error running command: ", err, ": ", stderr.String())
+		os.Exit(1)
+	}
+}
+
+func startScanning() {
+	r := os.Stdin
+	continue_scanning := true
+	page_counter := 1
+	text := "Scanning page %d\n"
+	for continue_scanning {
+		fmt.Printf(text, page_counter)
+
+		filename := time.Now().Format("20060102150405") + ".tiff"
+		files = append(files, filename)
+
+		scanimage_args := []string{
+			"scanimage", "--device-name 'genesys:libusb:001:011'", "--mode Color",
+			"--resolution 300 -x 215.9 -y 279.4", "--format=tiff", "-p", ">", filename,
+		}
+		cmd := exec.Command("bash", "-c", strings.Join(scanimage_args, " "))
+		runCommand(cmd)
+
+		continue_scanning = promptUser(r)
+		page_counter++
+	}
+}
+
+func promptUser(r io.Reader) bool {
+	snr := bufio.NewScanner(r)
+	for {
+		fmt.Print("Scan more pages? [Y/n] ")
+		snr.Scan()
+		response := snr.Text()
+		response = strings.ToLower(response)
+		if response == "" || yesre.MatchString(response) {
+			return true
+		} else if nore.MatchString(response) {
+			return false
+		}
+	}
+	return false
+}
+
+func promptForFilename(r io.Reader) string {
+	snr := bufio.NewScanner(r)
+	var response string
+	for {
+		fmt.Print("Enter pdf filename, no extension: ")
+		snr.Scan()
+		response = snr.Text()
+		if response != "" {
+			response = strings.Trim(verifyFilenameIncludesDate(response), " ")
+			if verifyFileNotExist(response) {
+				break
+			}
+		}
+	}
+	return response
+}
+
+func verifyFilenameIncludesDate(filename string) string {
+	result := filename
+	if !datere.MatchString(filename) {
+		result = time.Now().Format("2006-01-02_") + filename
+		fmt.Printf("Filename did not include a date, the new filename is %s.pdf\n", result)
+	}
+	return result
+}
+
+func verifyFileNotExist(filename string) bool {
+	if _, err := os.Stat(filename + ".pdf"); os.IsNotExist(err) {
+		// file does not already exist
+		return true
+	}
+	fmt.Printf("File already exists, with nanme %s.pdf pick another\n", filename)
+	return false
+}
+
+func convertFilesIntoNewTiffFiles() {
+	fmt.Println("Converting tiff files into scan files")
+	for i, file := range files {
+		output_filename := fmt.Sprintf("scan%03d.tiff", i+1)
+		scanfiles = append(scanfiles, output_filename)
+
+		command_string := []string{
+			"convert", file, "-deskew 40%", "-background white", "-level 10%,70%,1",
+			"-blur 2", "+dither", "+repage", "+matte", "-compress Group4", "-colorspace gray",
+			"-format tiff", output_filename,
+		}
+		cmd := exec.Command("bash", "-c", strings.Join(command_string, " "))
+		runCommand(cmd)
+	}
+	fmt.Println("Converting Done")
+}
+
+func createFinalPdfDocument(filename string) {
+	fmt.Println("Converting tiff scans to pdf")
+	pdfName := filename + ".pdf"
+	scanfiles = append(scanfiles, "doc.tiff")
+	cmd := exec.Command("tiffcp", scanfiles...)
+	runCommand(cmd)
+	cmd = exec.Command("tiff2pdf", "-j", "-o", pdfName, "doc.tiff")
+	runCommand(cmd)
+	fmt.Println("Convert Done")
+}
+
+func removeIntermediateFiles() {
+	files_to_remove := append(files, scanfiles...)
+	for _, file := range files_to_remove {
+		cmd := exec.Command("rm", "-f", file)
+		runCommand(cmd)
+	}
+	fmt.Println("Cleanup Done")
+}
---
+++ main_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestPromptUser(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected bool
+	}{
+		{"", true},
+		{"y", true},
+		{"yes", true},
+		{"Y", true},
+		{"YES", true},
+		{"Yes", true},
+		{"n", false},
+		{"no", false},
+		{"N", false},
+		{"NO", false},
+		{"No", false},
+		{`t
+y`, true},
+		{`quit
+y`, true},
+	}
+	for _, test := range tests {
+		r := strings.NewReader(test.input)
+		result := promptUser(r)
+		if result != test.expected {
+			t.Errorf("Expected %s but got %s", test.expected, result)
+		}
+	}
+}
+
+func TestPromptForFilename(t *testing.T) {
+	var date = time.Now().Format("2006-01-02_")
+	var test_files = []string{date + "existing1.pdf", "2015-01-01existing2.pdf"}
+	helperCreateExistingTestFiles(test_files)
+	defer helperRemoveExistingTestFiles(test_files)
+	var tests = []struct {
+		input    string
+		expected string
+	}{
+		{"unittest1", date + "unittest1"},
+		{"2014-08-01_unittest1", "2014-08-01_unittest1"},
+		{"2014-08-01_unittest1", "2014-08-01_unittest1"},
+		{`existing1
+unittest3`, date + "unittest3"},
+		{`2015-01-01existing2
+2015-01-01 unittest4`, "2015-01-01 unittest4"},
+	}
+	for _, test := range tests {
+		r := strings.NewReader(test.input)
+		result := promptForFilename(r)
+		if result != test.expected {
+			t.Errorf("Expected %s but got %s", test.expected, result)
+		}
+	}
+}
+
+func TestVerifyFilenameIncludesDate(t *testing.T) {
+	var date = time.Now().Format("2006-01-02_")
+	var tests = []struct {
+		input    string
+		expected string
+	}{
+		{"2014-01-01 newfile", "2014-01-01 newfile"},
+		{"2014-01 newfile", "2014-01 newfile"},
+		{"2014-01-01newfile", "2014-01-01newfile"},
+		{"2014-01newfile", "2014-01newfile"},
+		{"newfile", date + "newfile"},
+	}
+	for _, test := range tests {
+		result := verifyFilenameIncludesDate(test.input)
+		if result != test.expected {
+			t.Errorf("Expected %s but got %s", test.expected, result)
+		}
+	}
+}
+
+func TestVerifyFileNotExist(t *testing.T) {
+	var test_files = []string{"existing1.pdf", "2015-01-01existing2.pdf"}
+	helperCreateExistingTestFiles(test_files)
+	defer helperRemoveExistingTestFiles(test_files)
+	var tests = []struct {
+		input    string
+		expected bool
+	}{
+		{"existing1", false},
+		{"2015-01-01existing2", false},
+		{"untitest1", true},
+	}
+	for _, test := range tests {
+		result := verifyFileNotExist(test.input)
+		if result != test.expected {
+			t.Errorf("Expected %s but got %s", test.expected, result)
+		}
+	}
+}
+
+func helperCreateExistingTestFiles(files []string) {
+	for _, file := range files {
+		cmd := exec.Command("bash", "-c", "touch "+file)
+		runCommand(cmd)
+	}
+}
+
+func helperRemoveExistingTestFiles(files []string) {
+	for _, file := range files {
+		cmd := exec.Command("rm", "-f", file)
+		runCommand(cmd)
+	}
+}
