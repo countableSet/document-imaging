f9b1b69f79b4a3ffd8adcf328d0833636c3ae203
Rachel Aurand <rachel@countableset.com>
2016-08-16 21:24:12 -0700

    #3 add tests for config support and fmt

--- config.go
+++ config.go
@@ -2,44 +2,42 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/user"
 	"os/exec"
+	"os/user"
 	"regexp"
-	"fmt"
 )
 
 var (
-	configLocation = ".config"+string(os.PathSeparator)+"document-imaging"+string(os.PathSeparator)+"scanner.json"
-	configPath = ".config"+string(os.PathSeparator)+"document-imaging"
-	conf = &config{}
-	scanImageCmd = exec.Command("scanimage", "-L")
-	scanImageRe = regexp.MustCompile("^device `(?P<name>.*)'")
+	configLocation  = ".config" + string(os.PathSeparator) + "document-imaging" + string(os.PathSeparator) + "scanner.json"
+	configPath      = ".config" + string(os.PathSeparator) + "document-imaging"
+	conf            = &config{}
+	scanImageCmd    = exec.Command("scanimage", "-L")
+	scanImageRe     = regexp.MustCompile("^device `(?P<name>.*)'")
 	scanImageFailRe = regexp.MustCompile("^scanimage: open of device (.*) failed")
 )
 
 // Config struct to store scanner id information
 // should look something like
-// {
-//    scannerId: "genesys:libusb:001:011"
-// }
+// { scannerId: "genesys:libusb:001:011" }
 type config struct {
 	ScannerId string `json:"scannerId"`
 }
 
+// Fetches the scanner id from the config file, if the id is empty, fetch the id from the command and return that
 func fetchScannerIdFromConfig() string {
-	load()
+	usr, _ := user.Current()
+	configFile := usr.HomeDir + string(os.PathSeparator) + configLocation
+	load(configFile)
 	return verifyConfig()
 }
 
-// Load the JSON config file
-func load() {
-	usr, _ := user.Current()
-	configFile := usr.HomeDir+string(os.PathSeparator)+configLocation
-
+// Load the JSON config file from the provided path, and unmarshal into conf object
+func load(configFile string) {
 	var err error
 	var input = io.ReadCloser(os.Stdin)
 	if input, err = os.Open(configFile); err != nil {
@@ -60,7 +58,8 @@ func load() {
 	}
 }
 
-// device `genesys:libusb:001:002' is a Canon LiDE 200 flatbed scanner
+// Verifies the config information, if the id is empty, it will fetch it and return that
+// otherwise returns id from the config
 func verifyConfig() string {
 	var result string
 	if conf.ScannerId == "" {
@@ -71,6 +70,8 @@ func verifyConfig() string {
 	return result
 }
 
+// Fetches the scanner id from the `scanimage -L` command, write it to the config file and returns it
+// Command output: device `genesys:libusb:001:002' is a Canon LiDE 200 flatbed scanner
 func fetchScannerId() string {
 	output := runCommandWithOutput(scanImageCmd)
 	if !scanImageRe.MatchString(output) {
@@ -84,7 +85,8 @@ func fetchScannerId() string {
 	return match
 }
 
-// scanimage: open of device genesys:libusb:001:011 failed: Invalid argument
+// Verifies the command output from scanning to determine if the scanner id is still valid
+// Error output: scanimage: open of device genesys:libusb:001:011 failed: Invalid argument
 func verifyScanCommandOutput(output string) (bool, string) {
 	match := scanImageFailRe.FindString(output)
 	if match == "" {
@@ -94,21 +96,21 @@ func verifyScanCommandOutput(output string) (bool, string) {
 	}
 }
 
+// Write the conf object to the config file in json format for reference
 func writeConfigToFile() {
-	fmt.Println(conf.ScannerId)
 	jsonOutput, err := json.Marshal(conf)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Cannot marshal config json!", err)
 		os.Exit(1)
 	}
 	usr, _ := user.Current()
-	path := usr.HomeDir+string(os.PathSeparator)+configPath
+	path := usr.HomeDir + string(os.PathSeparator) + configPath
 	err = os.MkdirAll(path, 0777)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error create path directories!", err)
 		os.Exit(1)
 	}
-	configFile := usr.HomeDir+string(os.PathSeparator)+configLocation
+	configFile := usr.HomeDir + string(os.PathSeparator) + configLocation
 	err = ioutil.WriteFile(configFile, jsonOutput, 0666)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error writing to config json file!", err)
--- config_test.go
+++ config_test.go
@@ -1,11 +1,63 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"testing"
+)
 
 func TestLoad(t *testing.T) {
-	// todo
+	location := "load-test-file.json"
+	defer func() {
+		os.Remove(location)
+	}()
+	load(location)
+	if conf.ScannerId != "" {
+		t.Errorf("Expected not nil but got %s", conf.ScannerId)
+	}
+	id := "test"
+	testConf := config{ScannerId: id}
+	jsonOutput, _ := json.Marshal(testConf)
+	ioutil.WriteFile(location, jsonOutput, 0666)
+	load(location)
+	if conf.ScannerId != id {
+		t.Errorf("Expected %s but got %s", id, conf.ScannerId)
+	}
 }
 
 func TestWriteConfigToFile(t *testing.T) {
-	// todo
+	usr, _ := user.Current()
+	configLocation = ".config" + string(os.PathSeparator) + "document-imaging" + string(os.PathSeparator) + "scanner-testing.json"
+	location := usr.HomeDir + string(os.PathSeparator) + configLocation
+	defer func() {
+		configLocation = ".config" + string(os.PathSeparator) + "document-imaging" + string(os.PathSeparator) + "scanner.json"
+		os.Remove(location)
+	}()
+	conf.ScannerId = ""
+	writeConfigToFile()
+	if _, err := os.Stat(location); os.IsNotExist(err) {
+		t.Errorf("Expected path to exist %s", location)
+	}
+	if conf.ScannerId != "" {
+		t.Errorf("Expected empty string but got %s", conf.ScannerId)
+	}
+	load(location)
+	if conf.ScannerId != "" {
+		t.Errorf("Expected empty string but got %s", conf.ScannerId)
+	}
+	id := "test"
+	conf.ScannerId = id
+	writeConfigToFile()
+	if _, err := os.Stat(location); os.IsNotExist(err) {
+		t.Errorf("Expected path to exist %s", location)
+	}
+	if conf.ScannerId != id {
+		t.Errorf("Expected %s but got %s", id, conf.ScannerId)
+	}
+	load(location)
+	if conf.ScannerId != id {
+		t.Errorf("Expected %s but got %s", id, conf.ScannerId)
+	}
 }
