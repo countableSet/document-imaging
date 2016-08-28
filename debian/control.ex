29da534b2467304deacc810e19396446d360beda
Rachel Aurand <rachel@countableset.com>
2016-08-28 16:23:48 -0700

    #4 fix missing dependency for sane-utils

--- debian/control.ex
+++ debian/control.ex
@@ -3,5 +3,5 @@ Maintainer: countableset <rachel@countableset.com>
 
 Package: <name>
 Architecture: amd64
-Depends: imagemagick (>= 8:6.8.9.9), libtiff-tools (>= 4.0.6)
+Depends: imagemagick (>= 8:6.8.9.9), libtiff-tools (>= 4.0.6), sane-utils (>= 1.0.25+git20150528)
 Description: Scans documents.
